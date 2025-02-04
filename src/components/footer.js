eef1e8637db271a04780db7ad7f6bad0e599513e
imihhail <ivarton@hotmail.com>
2025-02-04 22:56:23 +0200

    'workingFooter'

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,15 +5,38 @@ import { SiGmail } from "react-icons/si";
 
 function Footer() {
 
+const icons = [
+    { component: FaGithub, link: "https://github.com/imihhail" },
+    { component: BsLinkedin, link: "https://linkedin.com/in/your-profile" },
+    { component: FaFacebookSquare, link: "https://facebook.com/your-profile" },
+    { component: SiGmail, link: "mailto:ivar.mihhailov@gmail.com" }
+];
+
 return (
     <div className="footerSection">
         <div className="bottomLine">
         </div>
         <div className="mediaIcons">
-         <FaGithub className='icon'/>
-         <BsLinkedin className='icon'/>
-         <FaFacebookSquare className='icon' />
-         <SiGmail className='icon'/>
+            <div className='iconArray1'>
+                {icons.map((icon, index) => {
+                    const IconComponent = icon.component;
+                        return (
+                            <a key={index} href={icon.link} target="_blank" rel="noopener noreferrer">
+                                <IconComponent className='icon' />
+                            </a>
+                        );
+                    })}
+            </div>
+            <div className='iconArray2'>
+                {icons.map((icon, index) => {
+                    const IconComponent = icon.component;
+                        return (
+                            <a key={index} href={icon.link} target="_blank" rel="noopener noreferrer">
+                                <IconComponent className='icon' />
+                            </a>
+                        );
+                    })}
+            </div>
         </div>
     </div>
     )
