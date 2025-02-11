b8dafeaffe6dee03a090bb6e89e1275a92a91a60
imihhail <ivarton@hotmail.com>
2025-02-11 22:01:33 +0200

    'image'

--- src/components/models.js
+++ src/components/models.js
@@ -1,48 +1,114 @@
+import React, { useState } from 'react';
 import './models.css';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
-import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing';
+import { EffectComposer, Bloom, ToneMapping, HueSaturation } from '@react-three/postprocessing';
 import pagodaGLB from '../assets/SM_Pagoda.glb';
-import customHDRI from '../assets/chinese.exr';
+import bell from '../assets/bell.jpg';
+import closerLook from '../assets/closer.jpg';
+import sunSet from '../assets/sunSet.jpg';
+import wireFrame from '../assets/wireFrame.png';
+import customHDRI from '../assets/garden.exr';
 import { RiDragMoveLine } from "react-icons/ri";
-import * as THREE from 'three';
-import { HueSaturation } from '@react-three/postprocessing';
-
-
+import { SiUnrealengine } from "react-icons/si";
+import { BiLogoBlender } from "react-icons/bi";
+import { ImEnlarge } from "react-icons/im";
 
 function Models() {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
-function Model() {
+  const handleMouseDown = (event) => {
+    if (event.button === 0 || event.button === 2) {
+      setIsMouseDown(true);
+    }
+  };
+
+  const handleMouseUpOrLeave = () => {
+    setIsMouseDown(false);
+  };
+
+  function Model() {
     const { scene } = useGLTF(pagodaGLB);
     return <primitive object={scene} scale={0.5} castShadow receiveShadow />;
   }
 
+  const handleImageClick = (src, alt) => {
+    setSelectedImage({ src, alt });
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
 
   return (
-    <div className="canvas-container">
-      <RiDragMoveLine className='rotateIcon'/>
-      <Canvas shadows camera={{ position: [0, 0, 8], fov: 50 }} dpr={[1, 2]} gl={{ antialias: true }} >
-        <ambientLight intensity={0.4} />
-        <directionalLight position={[5, 5, 5]} intensity={1.5} castShadow />
-        <spotLight position={[2, 5, 3]} angle={0.3} intensity={2} castShadow />
-        <Model />
-        <OrbitControls
-                enableDamping
-                autoRotate
-                autoRotateSpeed={0.5}
-                target={[0, 1, 0]}
+    <div className='modelsContainer'>
+      <div
+        className="canvas-container"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+      >
+        {!isMouseDown && <RiDragMoveLine className="rotateIcon" />}
+        {/* Blender icon at bottom left */}
+        <BiLogoBlender className="blenderIcon" />
+        <Canvas shadows camera={{ position: [0, 0, 8], fov: 50 }} dpr={[1, 2]} gl={{ antialias: true }}>
+          <ambientLight intensity={0.4} />
+          <directionalLight position={[5, 5, 5]} intensity={3} castShadow />
+          <spotLight position={[2, 5, 3]} angle={0.3} intensity={2} castShadow />
+          <Model />
+          <OrbitControls
+            enableDamping
+            autoRotate
+            autoRotateSpeed={0.5}
+            target={[0, 1, 0]}
+          />
+          <Environment files={customHDRI} background={false} />
+          <EffectComposer>
+            <HueSaturation saturation={0.12} />
+            <ToneMapping />
+            <Bloom
+              intensity={0.05}
+              luminanceThreshold={0.1}
+              luminanceSmoothing={0.025}
+              radius={0.6}
             />
-        <Environment files={customHDRI} background={false} rotation={[0, Math.PI / 2, 0]} />
-        <EffectComposer>
-        <HueSaturation saturation={0.12} />
-          <ToneMapping />
-          <Bloom intensity={0.05} luminanceThreshold={0.1} luminanceSmoothing={0.025} radius={0.6} />
-        </EffectComposer>
-        
-      </Canvas>
-      
+          </EffectComposer>
+        </Canvas>
+      </div>
+      <div className='imageContainer'>
+        <p>
+          Japanese pagoda inspired by Chureito pagoda near Mount Fuji. Model made with Blender and textured with Adobe Substance Painter.
+        </p>
+        <div className="imageWithIcon" onClick={() => handleImageClick(bell, "Bell")}>
+          <img src={bell} alt="Bell" />
+          <SiUnrealengine className="unrealIcon" />
+          <ImEnlarge className="enlargeIcon" />
+        </div>
+        <div className="imageWithIcon" onClick={() => handleImageClick(closerLook, "Closer Look")}>
+          <img src={closerLook} alt="Closer Look" />
+          <SiUnrealengine className="unrealIcon" />
+          <ImEnlarge className="enlargeIcon" />
+        </div>
+        <div className="imageWithIcon" onClick={() => handleImageClick(sunSet, "Sun Set")}>
+          <img src={sunSet} alt="Sun Set" />
+          <SiUnrealengine className="unrealIcon" />
+          <ImEnlarge className="enlargeIcon" />
+        </div>
+        {/* Uncomment to include wireframe image
+        <div className="imageWithIcon" onClick={() => handleImageClick(wireFrame, "Wireframe")}>
+          <img src={wireFrame} alt="Wireframe" />
+          <SiUnrealengine className="unrealIcon" />
+        </div>
+        */}
+      </div>
+      {selectedImage && (
+        <div className="modal" onClick={handleCloseModal}>
+          <img src={selectedImage.src} alt={selectedImage.alt} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Models;
